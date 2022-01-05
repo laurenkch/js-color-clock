184490482bc3bea288a8157fe857fb0f167c03d5
laurenkch <lkoch879@gmail.com>
2022-01-05 13:13:06 -0500

    logging every second in the console

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+//console log the current time
+
 let date = new Date();
 let hours = date.getHours(); 
 let minutes = date.getMinutes(); 
 let seconds = date.getSeconds();
-let time = hours + ":" + minutes + ":" + seconds;
-console.log(time);
+    time = hours + ":" + minutes + ":" + seconds;
+    console.log(time);
+
+//console log the time every second.
+
+setInterval(function(){
+    let date = new Date();
+    let hours = date.getHours(); 
+    let minutes = date.getMinutes(); 
+    let seconds = date.getSeconds();
+    time = hours + ":" + minutes + ":" + seconds;
+    console.log(time);
+}, 1000);
