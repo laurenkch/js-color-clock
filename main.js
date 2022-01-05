727d0a04ff1f2934ca5d8c7ecbf7e3de15294b9c
laurenkch <lkoch879@gmail.com>
2022-01-05 12:45:08 -0500

    logging current time

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var time = function () {
-    getSeconds;
-}
+let date = new Date();
+let hours = date.getHours(); 
+let minutes = date.getMinutes(); 
+let seconds = date.getSeconds();
+let time = hours + ":" + minutes + ":" + seconds;
 console.log(time);
