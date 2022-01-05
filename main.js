73b22415230df1e9edfbd0c686f1e133580e1733
laurenkch <lkoch879@gmail.com>
2022-01-05 14:59:29 -0500

    display the time every second

--- main.js
+++ main.js
@@ -1,19 +1,45 @@
+(function() {
+'use strict';
+
 //console log the current time
 
-let date = new Date();
-let hours = date.getHours(); 
-let minutes = date.getMinutes(); 
-let seconds = date.getSeconds();
-    time = hours + ":" + minutes + ":" + seconds;
-    console.log(time);
+// let date = new Date();
+// let hours = date.getHours(); 
+// let minutes = date.getMinutes(); 
+// let seconds = date.getSeconds();
+//     time = hours + ":" + minutes + ":" + seconds;
+//     console.log(time);
 
 //console log the time every second.
 
-setInterval(function(){
+// setInterval(function(){
+//     let date = new Date();
+//     let hours = date.getHours(); 
+//     let minutes = date.getMinutes(); 
+//     let seconds = date.getSeconds();
+//     time = hours + ":" + minutes + ":" + seconds;
+//     console.log(time);
+// }, 1000);
+
+//display current time everysecond
+
+let currentTime = function() {
     let date = new Date();
     let hours = date.getHours(); 
     let minutes = date.getMinutes(); 
     let seconds = date.getSeconds();
-    time = hours + ":" + minutes + ":" + seconds;
-    console.log(time);
-}, 1000);
+    return hours + ":" + minutes + ":" + seconds;
+};
+
+const timeText = document.querySelector(".clock-display");
+
+function displayTime() {
+ timeText.textContent = currentTime();
+};
+
+setInterval(displayTime, 1000);
+
+// 
+
+
+}) ();
